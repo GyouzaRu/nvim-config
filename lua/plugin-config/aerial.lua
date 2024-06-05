f3ce41dc8a1919c5097ac2bc12200a97c8cbd64d
GyouzaRu <lu291567698@hotmail.com>
2024-06-05 14:29:49 +0800

    refactor: Modify aerial setting

--- lua/plugin-config/aerial.lua
+++ lua/plugin-config/aerial.lua
@@ -7,7 +7,46 @@ end
 local keybindings = require('keybindings').aerial
 
 aerial.setup({
-  on_attach = keybindings.functionKeys_onattach()
+  -- Priority list of preferred backends for aerial.
+  -- This can be a filetype map (see :help aerial-filetype-map)
+  backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+
+  on_attach = keybindings.functionKeys_onattach(),
+
+  -- Show box drawing characters for the tree hierarchy
+  show_guides = true,
+
+  -- A list of all symbols to display. Set to false to display all symbols.
+  -- This can be a filetype map (see :help aerial-filetype-map)
+  -- To see all available values, see :help SymbolKind
+  filter_kind = {
+    "Array",
+    -- "Boolean",
+    "Class",
+    "Constant",
+    "Constructor",
+    "Enum",
+    -- "EnumMember",
+    "Event",
+    "Field",
+    "File",
+    "Function",
+    "Interface",
+    "Key",
+    "Method",
+    "Module",
+    "Namespace",
+    "Null",
+    -- "Number",
+    "Object",
+    "Operator",
+    "Package",
+    "Property",
+    -- "String",
+    "Struct",
+    "TypeParameter",
+    -- "Variable",
+  }
 })
 
 keybindings.functionKeys()

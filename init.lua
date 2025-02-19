fb552988e8192735c48b492be57babdfba2440ce
lujw2 <lujw2@lenovo.com>
2025-02-19 21:35:28 +0800

    refactor: move lazy.nvim setting to lua/lazy-nvim.lua

--- init.lua
+++ init.lua
@@ -7,19 +7,6 @@ require('autocmds')
 require('usercmds')
 
 -- lazy.nvim plugins management
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 require('lazy-nvim')
 
 -- GUI support

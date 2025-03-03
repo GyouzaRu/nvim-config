875c7ec789c9e4560e43d292abfb43120a8a50c9
GyouzaRu <lu291567698@hotmail.com>
2025-03-03 16:44:14 +0800

    refactor: remove mason-lspconfig

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,11 +4,11 @@ if not mason_status then
   return
 end
 
-local mason_config_status, mason_config = pcall(require, "mason-lspconfig")
-if not mason_config_status then
-  vim.notify("没有找到 mason-lspconfig")
-  return
-end
+-- local mason_config_status, mason_config = pcall(require, "mason-lspconfig")
+-- if not mason_config_status then
+--   vim.notify("没有找到 mason-lspconfig")
+--   return
+-- end
 
 -- :h mason-default-settings
 mason.setup({
@@ -23,17 +23,17 @@ mason.setup({
 
 -- mason-lspconfig uses the `lspconfig` server names in the APIs it exposes - not `mason.nvim` package names
 -- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
-mason_config.setup({
-  -- 确保安装，根据需要填写
-  ensure_installed = {
-    "clangd",
-    "cmake",
-    "lua_ls",
-    "bashls",
-    "dockerls",
-    "pyright",
-    "jsonls",
-    "yamlls",
-    -- "jdtls",
-  },
-})
+-- mason_config.setup({
+--   -- 确保安装，根据需要填写
+--   ensure_installed = {
+--     "clangd",
+--     -- "cmake",
+--     "lua_ls",
+--     -- "bashls",
+--     -- "dockerls",
+--     -- "pyright",
+--     -- "jsonls",
+--     -- "yamlls",
+--     -- "jdtls",
+--   },
+-- })
